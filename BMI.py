665fb85b7cf74d3321a62189f6eb8b98cdf605c3
alimuavia0 <alimuaviashah100010@gmail.com>
2026-01-20 13:56:53 +0500

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,19 @@
+height = float(input("Please enter height value (meters): "))
+weight = float(input("Please enter weight value (kg): "))
+
+bmi = weight / (height**2)
+
+print(bmi)
+
+# if bmi < 18.5 :
+#     print("BMI: Under Weight")
+# else :
+#     if bmi < 24.5 :
+#         print("BMI: Normal Weight")
+#     else :
+#         if bmi < 30 :
+#              print("BMI: Over Weight")
+#         else :
+#             print("BMI: Obesity")
+            
+        
